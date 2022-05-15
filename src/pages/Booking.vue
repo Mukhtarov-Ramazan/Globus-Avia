23eb7811eee36c32f1544ab8cb9410ee7828a00f
Mukhtarov-Ramazan <mukhtarovramazan239@gmail.com>
2022-05-15 19:09:23 +0300

    adding and removing passengers

--- src/pages/Booking.vue
+++ src/pages/Booking.vue
@@ -22,47 +22,90 @@
           <th>Cost</th>
         </tr>
         <tr>
-          <td class="test-5-fc">{{FLIGHTS.data.flights_to[this.$route.query.indexTo].flight_code}}</td>
-          <td>
-            <span class="test-5-from-cn">{{FLIGHTS.data.flights_to[this.$route.query.indexTo].from.city}}</span>,
-            <span class="test-5-from-an">{{FLIGHTS.data.flights_to[this.$route.query.indexTo].from.airport}}</span>
+          <td class="test-5-fc">
+            {{ FLIGHTS.data.flights_to[this.$route.query.indexTo].flight_code }}
           </td>
           <td>
-            <span class="test-5-dd">{{FLIGHTS.data.flights_to[this.$route.query.indexTo].from.date}}</span>
+            <span class="test-5-from-cn">{{
+              FLIGHTS.data.flights_to[this.$route.query.indexTo].from.city
+            }}</span
+            >,
+            <span class="test-5-from-an">{{
+              FLIGHTS.data.flights_to[this.$route.query.indexTo].from.airport
+            }}</span>
+          </td>
+          <td>
+            <span class="test-5-dd">{{
+              FLIGHTS.data.flights_to[this.$route.query.indexTo].from.date
+            }}</span>
             at
-            <span class="test-5-dt">{{FLIGHTS.data.flights_to[this.$route.query.indexTo].from.time}}</span>
+            <span class="test-5-dt">{{
+              FLIGHTS.data.flights_to[this.$route.query.indexTo].from.time
+            }}</span>
           </td>
           <td class="test-5-to">
-            <span class="test-5-to-cn">{{FLIGHTS.data.flights_to[this.$route.query.indexTo].to.date}}</span>,
-            <span class="test-5-to-an">{{FLIGHTS.data.flights_to[this.$route.query.indexTo].to.airport}}</span>
+            <span class="test-5-to-cn">{{
+              FLIGHTS.data.flights_to[this.$route.query.indexTo].to.date
+            }}</span
+            >,
+            <span class="test-5-to-an">{{
+              FLIGHTS.data.flights_to[this.$route.query.indexTo].to.airport
+            }}</span>
           </td>
-          <td class="test-5-at">{{FLIGHTS.data.flights_to[this.$route.query.indexTo].to.time}}</td>
-          <td class="test-5-fp">{{FLIGHTS.data.flights_to[this.$route.query.indexTo].cost}}</td>
+          <td class="test-5-at">
+            {{ FLIGHTS.data.flights_to[this.$route.query.indexTo].to.time }}
+          </td>
+          <td class="test-5-fp">
+            {{ FLIGHTS.data.flights_to[this.$route.query.indexTo].cost }}
+          </td>
         </tr>
-        
+
         <tr>
-          <td class="test-5-fc">{{FLIGHTS.data.flights_back[this.$route.query.indexBack].flight_code}}</td>
-          <td>
-            <span class="test-5-from-cn">{{FLIGHTS.data.flights_back[this.$route.query.indexBack].from.city}}</span>,
-            <span class="test-5-from-an">{{FLIGHTS.data.flights_back[this.$route.query.indexBack].from.airport}}</span>
+          <td class="test-5-fc">
+            {{
+              FLIGHTS.data.flights_back[this.$route.query.indexBack].flight_code
+            }}
           </td>
           <td>
-            <span class="test-5-dd">{{FLIGHTS.data.flights_back[this.$route.query.indexBack].from.date}}</span>
+            <span class="test-5-from-cn">{{
+              FLIGHTS.data.flights_back[this.$route.query.indexBack].from.city
+            }}</span
+            >,
+            <span class="test-5-from-an">{{
+              FLIGHTS.data.flights_back[this.$route.query.indexBack].from
+                .airport
+            }}</span>
+          </td>
+          <td>
+            <span class="test-5-dd">{{
+              FLIGHTS.data.flights_back[this.$route.query.indexBack].from.date
+            }}</span>
             at
-            <span class="test-5-dt">{{FLIGHTS.data.flights_back[this.$route.query.indexBack].from.time}}</span>
+            <span class="test-5-dt">{{
+              FLIGHTS.data.flights_back[this.$route.query.indexBack].from.time
+            }}</span>
           </td>
           <td class="test-5-to">
-            <span class="test-5-to-cn">{{FLIGHTS.data.flights_back[this.$route.query.indexBack].to.date}}</span>,
-            <span class="test-5-to-an">{{FLIGHTS.data.flights_back[this.$route.query.indexBack].to.airport}}</span>
+            <span class="test-5-to-cn">{{
+              FLIGHTS.data.flights_back[this.$route.query.indexBack].to.date
+            }}</span
+            >,
+            <span class="test-5-to-an">{{
+              FLIGHTS.data.flights_back[this.$route.query.indexBack].to.airport
+            }}</span>
           </td>
-          <td class="test-5-at">{{FLIGHTS.data.flights_back[this.$route.query.indexBack].to.time}}</td>
-          <td class="test-5-fp">{{FLIGHTS.data.flights_back[this.$route.query.indexBack].cost}}</td>
+          <td class="test-5-at">
+            {{ FLIGHTS.data.flights_back[this.$route.query.indexBack].to.time }}
+          </td>
+          <td class="test-5-fp">
+            {{ FLIGHTS.data.flights_back[this.$route.query.indexBack].cost }}
+          </td>
         </tr>
         <tr>
           <td colspan="5" class="text-right">
             <b>Total cost</b>
           </td>
-          <td colspan="1" class="text-right test-5-price">12333</td>
+          <td colspan="1" class="text-right test-5-price">{{ totalCost }}</td>
         </tr>
       </table>
     </section>
@@ -70,16 +113,17 @@
     <section class="mt-5">
       <div class="d-flex justify-content-between align-items-baseline mb-4">
         <h4 class="text-white">Passengers</h4>
-        <button class="btn btn-info btn-sm test-5-add">Add passenger</button>
+
       </div>
 
-      <form action="booking_management.html">
+      <form @submit.prevent>
         <div class="row">
           <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pr-lg-0">
             <input
               type="text"
               class="form-control test-5-name w-100"
               placeholder="First name"
+              v-model="form.firstName"
             />
           </div>
           <div class="col-12 col-sm-6 col-lg-4 mt-3 col-xl-3 mt-sm-0 pr-lg-0">
@@ -87,6 +131,7 @@
               type="text"
               class="form-control test-5-last w-100"
               placeholder="Last name"
+              v-model="form.lastName"
             />
           </div>
           <div class="col-12 col-sm-6 col-lg-2 mt-3 col-xl-2 mt-lg-0 pr-lg-0">
@@ -94,6 +139,7 @@
               type="text"
               class="form-control test-5-dob"
               placeholder="Date of Birth"
+              v-model="form.dateBirth"
             />
           </div>
           <div class="col-12 col-sm-6 col-lg-2 mt-3 col-xl-2 mt-lg-0 pr-xl-0">
@@ -101,11 +147,12 @@
               type="text"
               class="form-control test-5-doc"
               placeholder="Document number"
+              v-model="form.documentNamber"
             />
           </div>
           <div class="col-12 col-xl-2 mt-3 mt-xl-0">
-            <button class="btn btn-danger btn-sm form-control test-5-bremove">
-              Remove
+            <button class="btn btn-info btn-sm form-control test-5-remove" @click="addPassenger(this.form)">
+              Add passenger
             </button>
           </div>
         </div>
@@ -114,30 +161,81 @@
           Confirm
         </button>
       </form>
+
+      <table class="table text-white table-users" v-if="USERS.length">
+        <tr>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Date of Birth</th>
+          <th>Document number</th>
+          <th></th>
+        </tr>
+        <tr v-for="(user, index) in USERS" :key="index">
+          <td class="test-6-name">{{ user.firstName }}</td>
+          <td class="test-6-last">{{ user.lastName }}</td>
+          <td class="test-6-dob">{{ user.dateBirth }}</td>
+          <td class="test-6-doc">{{ user.documentNamber }}</td>
+          <td>
+            <button
+              class="btn btn-danger btn-info btn-sm test-5-add"
+              @click="removePassenger(index)"
+            >
+              Remove
+            </button>
+          </td>
+        </tr>
+      </table>
     </section>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-  data () {
-    return{
-      
-    }
+  data() {
+    return {
+      totalCost: "",
+      form: {
+        firstName: "",
+        lastName: "",
+        dateBirth: "",
+        documentNamber: "",
+      },
+    };
+  },
+
+  mounted() {
+    this.totalCost =
+      this.FLIGHTS.data.flights_back[this.$route.query.indexBack].cost +
+      this.FLIGHTS.data.flights_to[this.$route.query.indexTo].cost;
   },
 
   methods: {
-    totalCost () {
+    ...mapActions(["ADD_PASSENGERS", "REMOVE_PASSENGERS"]),
 
+    addPassenger() {
+      this.ADD_PASSENGERS(this.form);
+
+      this.form.firstName = "";
+      this.form.lastName = "";
+      this.form.dateBirth = "";
+      this.form.documentNamber = "";
+    },
+
+    removePassenger(index) {
+      this.REMOVE_PASSENGERS(index);
     }
   },
 
   computed: {
-    ...mapGetters(["FLIGHTS"])
-  }
+    ...mapGetters(["FLIGHTS", "USERS"]),
+  },
 };
 </script>
 
-<style></style>
+<style>
+.table-users {
+  margin-top: 50px;
+}
+</style>
--- src/store/models/bookingUser.js
+++ src/store/models/bookingUser.js
@@ -1,11 +1,33 @@
 export default {
    state: {
-      user: {}
+      users: []
    },
    
-   mutations: {},
+   mutations: {
+      ADD_PASS (state, newForm) {
+         state.users.push(newForm)
+      },
 
-   actions: {},
+      REMOVE_PASS (state, index) {
+         state.users.splice(index, 1)
+      }
+   },
 
-   getters: {},
+   actions: {
+      ADD_PASSENGERS ({commit}, form) {
+         const newForm = JSON.parse(JSON.stringify(form))
+         commit('ADD_PASS', newForm)
+      }, 
+
+      REMOVE_PASSENGERS ({commit}, index) {
+         
+         commit('REMOVE_PASS', index)
+      }
+   },
+
+   getters: {
+      USERS (state) {
+         return state.users
+      }
+   },
 }
